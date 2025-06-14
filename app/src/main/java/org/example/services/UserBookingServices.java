2b5c270e364736b387ab5dd27d27d8f3011514b1
Rajneesh Kumar <192125aryaraj@gmail.com>
2025-06-15 00:50:09 +0530

    Adding Initial File

---
+++ app/src/main/java/org/example/services/UserBookingServices.java
@@ -0,0 +1,10 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookingServices {
+    private User user;
+    public UserBookingServices(User user1){
+        this.user = user1;
+    }
+}
